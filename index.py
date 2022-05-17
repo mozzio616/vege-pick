9ed083cda8c70e545022420aff70eb7d3f32b706
mozzio616 <mozzio616@gmail.com>
2022-05-17 17:24:54 +0900

    v0.0.11 MongoDB

--- index.py
+++ index.py
@@ -1,15 +1,23 @@
+from multiprocessing import connection
 from flask import Flask, render_template, request, redirect
 import paypayopa
 from flask_qrcode import QRcode
 import os, datetime
-
-app = Flask(__name__)
-QRcode(app)
+import pymongo
 
 API_KEY = os.environ['PP_API_KEY']
 API_SECRET = os.environ['PP_API_SECRET']
 MERCHANT_ID = os.environ['PP_MERCHANT_ID']
 REDIRECT_URL = os.environ['PP_REDIRECT_URL']
+DB_USER = os.environ['MONGO_USER']
+DB_PASS = os.environ['MONGO_PASS']
+
+connection_url = 'mongodb+srv://' + DB_USER + ':' + DB_PASS + '@cluster0.gdlpa.gcp.mongodb.net/?retryWrites=true&w=majority'
+
+app = Flask(__name__)
+QRcode(app)
+mongoClient = pymongo.MongoClient(connection_url)
+db = mongoClient.lvl
 
 client = paypayopa.Client(auth=(API_KEY, API_SECRET), production_mode=False)
 client.set_assume_merchant(MERCHANT_ID)
@@ -36,73 +44,56 @@ def items():
     if locationId is None:
         return redirect('/search')
     else:
-        i = 0
-        while i < len(data):
-            if data[i]['locationId'] == locationId:
-                break
-            else:
-                i += 1
-        if i < len(data):
-            return render_template('items.html', data=data[i])
-        else:
+        locationData = db.locations.find_one({'locationId': locationId})
+        itemData = db.items.find({'locationId': locationId})
+        if locationData is None or itemData is None:
             return redirect('/search')
+        else:
+            return render_template('items.html', locationData=locationData, itemData=itemData)
 
 @app.route('/code', methods=['GET'])
 def create_code():
     locationId = request.args.get('locationId')
     itemId = request.args.get('itemId')
     if locationId is None or itemId is None:
-        return redirect('/sold')
+        return redirect('/search')
     else:
-        i = 0
-        while i < len(data):
-            if data[i]['locationId'] == locationId:
-                break
-            else:
-                i += 1
-        if i < len(data):
-            lockerItems = data[i]['lockerItems']
-            j = 0
-            while j < len(lockerItems):
-                if data[i]['lockerItems'][j]['itemId'] == itemId:
-                    break
-                else:
-                    j += 1
-            if j < len(lockerItems):
-                if lockerItems[j]['isAvailable'] is True:
-                    merchantPaymentId = create_merchant_payment_id()
-                    req = {
-                        "merchantPaymentId": merchantPaymentId,
-                        "codeType": "ORDER_QR",
-                        "redirectUrl": REDIRECT_URL,
-                        "redirectType":"WEB_LINK",
-                        "orderDescription": lockerItems[j]['itemName'],
-                        "orderItems": [{
-                            "name": lockerItems[j]['itemName'],
-                            "category": "vegetables",
-                            "quantity": 1,
-                            "productId": itemId,
-                            "unitPrice": {
-                                "amount": 1,
-                                "currency": "JPY"
-                            }
-                        }],
-                        "amount": {
-                            "amount": 1,
-                            "currency": "JPY"
-                        },
-                    }
-                    res = client.Code.create_qr_code(req)
-                    print(res['resultInfo']['code'])
-                    print(res['data']['url'])
-                    paypay_url = res['data']['url']
-                    return render_template('code.html', paypay_url = paypay_url)
-                else:
-                    return redirect('/sold')
-            else:
-                return redirect('/search')
-        else:
+        locationData = db.locations.find_one({'locationId': locationId})
+        itemData = db.items.find_one({'locationId': locationId, 'itemId': itemId})
+        if locationData is None:
             return redirect('/search')
+        elif itemData is None:
+            return redirect('/items?locationId=' + locationId)
+        elif itemData['isAvailable'] is True:
+            merchantPaymentId = create_merchant_payment_id()
+            req = {
+                "merchantPaymentId": merchantPaymentId,
+                "codeType": "ORDER_QR",
+                "redirectUrl": REDIRECT_URL,
+                "redirectType":"WEB_LINK",
+                "orderDescription": itemData['itemName'],
+                "orderItems": [{
+                    "name": itemData['itemName'],
+                    "category": "vegetables",
+                    "quantity": 1,
+                    "productId": itemId,
+                    "unitPrice": {
+                        "amount": 1,
+                        "currency": "JPY"
+                    }
+                }],
+                "amount": {
+                    "amount": 1,
+                    "currency": "JPY"
+                },
+            }
+            res = client.Code.create_qr_code(req)
+            print(res['resultInfo']['code'])
+            print(res['data']['url'])
+            paypay_url = res['data']['url']
+            return render_template('code.html', paypay_url=paypay_url, locationData=locationData, itemData=itemData)
+        else:
+            return redirect('/sold?locationId=' + locationId)
 
 @app.route('/thanks')
 def thanks():
@@ -110,355 +101,30 @@ def thanks():
 
 @app.route('/sold')
 def sold():
-    return render_template('sold.html')
+    locationId = request.args.get('locationId')
+    return render_template('sold.html', locationId=locationId)
 
-data = [
-    {
-        'locationId': '1001',
-        'locationNameEn': 'Komazawa-daigaku Sta.',
-        'locationNameJp': '駒沢大学駅',
-        'lockerItems': [
-            {
-                'lockerNo': 1,
-                'itemId': '1001001',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 2,
-                'itemId': '1001002',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 3,
-                'itemId': '1001003',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 4,
-                'itemId': '1001004',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 5,
-                'itemId': '1001005',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 6,
-                'itemId': '1001006',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 7,
-                'itemId': '1001007',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 8,
-                'itemId': '1001008',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 9,
-                'itemId': '1001009',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 10,
-                'itemId': '1001010',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 11,
-                'itemId': '1001011',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 12,
-                'itemId': '1001012',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': True
-            }
-        ]
-    },
-    {
-        'locationId': '1002',
-        'locationNameEn': 'Shin-machi Nursery School',
-        'locationNameJp': '新町保育園',
-        'lockerItems': [
-            {
-                'lockerNo': 1,
-                'itemId': '1002001',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 2,
-                'itemId': '1002002',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 3,
-                'itemId': '1002003',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 4,
-                'itemId': '1002004',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 5,
-                'itemId': '1002005',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 6,
-                'itemId': '1002006',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 7,
-                'itemId': '1002007',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 8,
-                'itemId': '1002008',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 9,
-                'itemId': '1002009',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 10,
-                'itemId': '1002010',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 11,
-                'itemId': '1002011',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 12,
-                'itemId': '1002012',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': True
-            },
-        ]
-    },
-    {
-        'locationId': '1003',
-        'locationNameEn': 'Fukasawa Elementary School',
-        'locationNameJp': '深沢小学校',
-        'lockerItems': [
-            {
-                'lockerNo': 1,
-                'itemId': '1003001',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 2,
-                'itemId': '1003002',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 3,
-                'itemId': '1003003',
-                'itemName': 'せたがやそだち キャベツ 1玉',
-                'itemDescription': '世田谷で育った新鮮なキャベツです。',
-                'itemPrice': 200,
-                'itemImg': 'cabbage.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 4,
-                'itemId': '1003004',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 5,
-                'itemId': '1003005',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 6,
-                'itemId': '1003006',
-                'itemName': 'せたがやそだち トマト 大3個',
-                'itemDescription': '世田谷で育った新鮮なトマトです。',
-                'itemPrice': 300,
-                'itemImg': 'tomato.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 7,
-                'itemId': '1003007',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 8,
-                'itemId': '1003008',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': True
-            },
-            {
-                'lockerNo': 9,
-                'itemId': '1003009',
-                'itemName': 'せたがやそだち たまねぎ 中3個',
-                'itemDescription': '世田谷で育った新鮮な玉ねぎです。',
-                'itemPrice': 300,
-                'itemImg': 'onion.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 10,
-                'itemId': '1003010',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 11,
-                'itemId': '1003011',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': False
-            },
-            {
-                'lockerNo': 12,
-                'itemId': '1003012',
-                'itemName': 'せたがやそだち 詰め合わせ',
-                'itemDescription': 'カレーにぴったりな、じゃがいも（中3個）、にんじん（1本）、たまねぎ（中3個）のセットです。',
-                'itemPrice': 500,
-                'itemImg': 'vegset1.png',
-                'isAvailable': True
-            }
-        ]
-    }
-]
+@app.route('/api/locations', methods=['GET', 'POST'])
+def addLocations():
+    collection = db.locations
+    if request.method == 'POST':
+        for index, location in enumerate(request.json):
+            collection.insert_one(location)
+        response = {'message': str(len(request.json)) + ' locations created.'}
+        return response
+    else:
+        return '{}'
+
+@app.route('/api/items', methods=['GET', 'POST'])
+def addItems():
+    collection = db.items
+    if request.method == 'POST':
+        for index, item in enumerate(request.json):
+            collection.insert_one(item)
+        response = {'message': str(len(request.json)) + ' items created.'}
+        return response
+    else:
+        return '{}'
 
 
 if __name__ == '__main__':
