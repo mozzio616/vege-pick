341d8c66b3eca1adb5f2bb9ba8d5761ff086e38c
mozzio616 <mozzio616@gmail.com>
2022-05-18 01:34:33 +0900

    v0.0.16 monitoring payment status and update stock

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import paypayopa
 from flask_qrcode import QRcode
-import os, datetime
+import os, datetime, time
+from threading import Thread
 import pymongo
 
 API_KEY = os.environ['PP_API_KEY']
@@ -13,18 +14,19 @@ DB_HOST = os.environ['MONGO_HOST']
 connection_url = DB_HOST
 
 app = Flask(__name__)
-QRcode(app)
+
 mongoClient = pymongo.MongoClient(connection_url)
 db = mongoClient.lvl
 locationCollection = db.locations
 itemCollection = db.items
 
+QRcode(app)
 client = paypayopa.Client(auth=(API_KEY, API_SECRET), production_mode=False)
 client.set_assume_merchant(MERCHANT_ID)
 
 def create_merchant_payment_id():
     dt = datetime.datetime.now()
-    return dt.strftime('%Y%m%d%H%M%S')
+    return dt.strftime('%Y%m%d%H%M')
 
 @app.route('/')
 def index():
@@ -45,13 +47,13 @@ def items():
         return redirect('/search')
     else:
         locationData = locationCollection.find_one({'locationId': locationId})
-        itemData = itemCollection.find({'locationId': locationId})
+        itemData = itemCollection.find({'locationId': locationId}).sort('itemId', pymongo.ASCENDING)
         if locationData is None or itemData is None:
             return redirect('/search')
         else:
             return render_template('items.html', locationData=locationData, itemData=itemData)
 
-@app.route('/code', methods=['GET'])
+@app.route('/code')
 def create_code():
     locationId = request.args.get('locationId')
     itemId = request.args.get('itemId')
@@ -89,11 +91,36 @@ def create_code():
             }
             res = client.Code.create_qr_code(req)
             print(res['resultInfo']['code'])
-            print(res['data']['url'])
-            paypay_url = res['data']['url']
-            return render_template('code.html', paypay_url=paypay_url, locationData=locationData, itemData=itemData)
+            resultCode = res['resultInfo']['code']
+            if resultCode == 'SUCCESS':
+                itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': False}})
+                print(res['data']['url'])
+                paypay_url = res['data']['url']
+                Thread(target=wait_checkout, args=(merchantPaymentId, itemId,)).start()
+                return render_template('code.html', paypay_url=paypay_url, locationData=locationData, itemData=itemData)
+            else:
+                return redirect('/items?locationId=' + locationId)
         else:
             return redirect('/sold?locationId=' + locationId)
+
+def wait_checkout(merchantPaymentId, itemId):
+    i = 0
+    while i < 50:
+        response = client.Code.get_payment_details(merchantPaymentId)
+        #print(response['data']['status'])
+        if response['data']['status'] == 'COMPLETED':
+            return print('paid')
+            break
+        elif response['data']['status'] != 'CREATED':
+            itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': True}})
+            return print('not paid')
+            break
+        else:
+            i += 1
+            time.sleep(10)
+    else:
+        itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': True}})
+        return print('timeout')
 
 @app.route('/thanks')
 def thanks():
