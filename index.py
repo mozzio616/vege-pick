6e83595045ed778a3a62c98de385799fab33ee90
mozzio616 <mozzio616@gmail.com>
2022-05-18 23:46:04 +0900

    v0.0.22 slack integration

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, redirect
-import paypayopa
 from flask_qrcode import QRcode
-import os, datetime, time
-from threading import Thread
+import paypayopa
 import pymongo
+from slack_sdk.webhook import WebhookClient
+import os, datetime
 
 API_KEY = os.environ['PP_API_KEY']
 API_SECRET = os.environ['PP_API_SECRET']
 MERCHANT_ID = os.environ['PP_MERCHANT_ID']
 REDIRECT_URL = os.environ['PP_REDIRECT_URL']
 DB_HOST = os.environ['MONGO_HOST']
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
 
 connection_url = DB_HOST
 
@@ -17,16 +18,17 @@ app = Flask(__name__)
 
 mongoClient = pymongo.MongoClient(connection_url)
 db = mongoClient.lvl
-locationCollection = db.locations
-itemCollection = db.items
+collection_locations = db.locations
+collection_items = db.items
 
 QRcode(app)
 client = paypayopa.Client(auth=(API_KEY, API_SECRET), production_mode=False)
 client.set_assume_merchant(MERCHANT_ID)
 
-def create_merchant_payment_id():
+def create_merchant_payment_id(itemId):
     dt = datetime.datetime.now()
-    return dt.strftime('%Y%m%d%H%M')
+    merchantPaymentId = itemId + dt.strftime('%Y%m%d%H%M%S')
+    return merchantPaymentId
 
 @app.route('/')
 def index():
@@ -41,17 +43,17 @@ def search():
     return render_template('search.html')
 
 @app.route('/items')
-def items():
+def get_items():
     locationId = request.args.get('locationId')
     if locationId is None:
         return redirect('/search')
     else:
-        locationData = locationCollection.find_one({'locationId': locationId})
-        itemData = itemCollection.find({'locationId': locationId}).sort([('itemId', pymongo.ASCENDING)])
-        if locationData is None or itemData is None:
+        location = collection_locations.find_one({'locationId': locationId})
+        items = collection_items.find({'locationId': locationId}).sort([('itemId', pymongo.ASCENDING)])
+        if location is None or items is None:
             return redirect('/search')
         else:
-            return render_template('items.html', locationData=locationData, itemData=itemData)
+            return render_template('items.html', location=location, items=items)
 
 @app.route('/code')
 def create_code():
@@ -60,23 +62,23 @@ def create_code():
     if locationId is None or itemId is None:
         return redirect('/search')
     else:
-        locationData = locationCollection.find_one({'locationId': locationId})
-        itemData = itemCollection.find_one({'locationId': locationId, 'itemId': itemId})
-        if locationData is None:
+        location = collection_locations.find_one({'locationId': locationId})
+        item = collection_items.find_one({'locationId': locationId, 'itemId': itemId})
+        if location is None:
             return redirect('/search')
-        elif itemData is None:
+        elif item is None:
             return redirect('/items?locationId=' + locationId)
-        elif itemData['isAvailable'] is True:
-            merchantPaymentId = create_merchant_payment_id()
+        elif item['isAvailable'] is True:
+            merchantPaymentId = create_merchant_payment_id(itemId)
             print(merchantPaymentId)
             req = {
                 "merchantPaymentId": merchantPaymentId,
                 "codeType": "ORDER_QR",
                 "redirectUrl": REDIRECT_URL,
                 "redirectType":"WEB_LINK",
-                "orderDescription": itemData['itemName'],
+                "orderDescription": item['itemName'],
                 "orderItems": [{
-                    "name": itemData['itemName'],
+                    "name": item['itemName'],
                     "category": "vegetables",
                     "quantity": 1,
                     "productId": itemId,
@@ -90,38 +92,19 @@ def create_code():
                     "currency": "JPY"
                 },
             }
-            res = client.Code.create_qr_code(req)
-            print(res['resultInfo']['code'])
-            resultCode = res['resultInfo']['code']
+            response = client.Code.create_qr_code(req)
+            resultCode = response['resultInfo']['code']
             if resultCode == 'SUCCESS':
-                itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': False}})
-                print(res['data']['url'])
-                paypay_url = res['data']['url']
-                Thread(target=wait_checkout, args=(merchantPaymentId, itemId,)).start()
-                return render_template('code.html', paypay_url=paypay_url, locationData=locationData, itemData=itemData)
+                res_db = collection_items.update_one({'itemId': itemId}, {'$set': {'isAvailable': False}})
+                message = merchantPaymentId + ':' + itemId
+                webhook = WebhookClient(WEBHOOK_URL)
+                res_webhook = webhook.send(text=message)
+                url = response['data']['url']
+                return render_template('code.html', url=url, location=location, item=item)
             else:
                 return redirect('/items?locationId=' + locationId)
         else:
             return redirect('/sold?locationId=' + locationId)
-
-def wait_checkout(merchantPaymentId, itemId):
-    i = 0
-    while i < 5:
-        response = client.Code.get_payment_details(merchantPaymentId)
-        #print(response['data']['status'])
-        if response['data']['status'] == 'COMPLETED':
-            return print('paid')
-            break
-        elif response['data']['status'] != 'CREATED':
-            itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': True}})
-            return print('not paid')
-            break
-        else:
-            i += 1
-            time.sleep(1)
-    else:
-        itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': True}})
-        return print('timeout')
 
 @app.route('/thanks')
 def thanks():
@@ -133,49 +116,52 @@ def sold():
     return render_template('sold.html', locationId=locationId)
 
 @app.route('/api/locations', methods=['GET', 'POST'])
-def addLocations():
-    if request.method == 'POST':
-        for index, location in enumerate(request.json):
-            locationCollection.insert_one(location)
+def api_locations():
+    if request.method == 'GET':
+        locations = collection_locations.find()
+        return locations
+    elif request.method == 'POST':
+        for i, location in enumerate(request.json):
+            collection_locations.insert_one(location)
         response = {'message': str(len(request.json)) + ' locations created.'}
         return response
     else:
         return '{}'
 
 @app.route('/api/items', methods=['GET', 'POST'])
-def addItems():
-    if request.method == 'POST':
-        for index, item in enumerate(request.json):
-            itemCollection.insert_one(item)
+def api_items():
+    if request.method == 'GET':
+        items = collection_items.find()
+        return items
+    elif request.method == 'POST':
+        for i, item in enumerate(request.json):
+            collection_items.insert_one(item)
         response = {'message': str(len(request.json)) + ' items created.'}
         return response
     else:
         return '{}'
 
-@app.route('/api/items/<itemId>', methods=['GET', 'PUT'])
-def updateStock(itemId):
-    if request.method == 'PUT':
+@app.route('/api/items/<itemId>/status', methods=['GET', 'PUT'])
+def item_status(itemId):
+    if request.method == 'GET':
+        item = collection_items.find_one({'itemId': itemId})
+        response = {'itemId': itemId, 'isAvailable': item['isAvailable']}
+        return response
+    elif request.method == 'PUT':
         isAvailable = request.json['isAvailable']
-        itemCollection.update_one({'itemId': itemId}, {'$set': {'isAvailable': isAvailable}})
+        item = collection_items.update_one({'itemId': itemId}, {'$set': {'isAvailable': isAvailable}})
         response = {'itemId': itemId, 'isAvailable': isAvailable}
         return response
     else:
-        itemData = itemCollection.find_one({'itemId': itemId})
-        response = {'itemId': itemId, 'isAvailable': itemData['isAvailable']}
-        return response
+        return {}
 
-@app.route('/api/webhook', methods=['GET', 'POST'])
-def webhook():
-    if request.method == 'POST':
-        return 'ok'
-    else:
-        return 'ok'
+@app.route('/api/payments/<merchantPaymentId>/status')
+def payment_status(merchantPaymentId):
+    payment_detail = client.Code.get_payment_details(merchantPaymentId)
+    status = payment_detail['data']['status']
+    response = {'merchantPaymentId': merchantPaymentId, 'status': status}
+    return response
 
-@app.route('/api/paypay/<merchantPaymentId>/status')
-def paypay_status(merchantPaymentId):
-    response = client.Code.get_payment_details(merchantPaymentId)
-    print(response['data']['status'])
-    return 'ok'
             
 if __name__ == '__main__':
     app.run()
