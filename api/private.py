cc5b685743a21353e9b43c3d066f49370d16b4f1
mozzio616 <mozzio616@gmail.com>
2022-06-18 01:55:16 +0900

    v0.0.70 update response message in private api

--- api/private.py
+++ api/private.py
@@ -9,7 +9,7 @@ api_private = Blueprint('api_private', __name__)
 @cross_origin(headers=["Access-Control-Allow-Origin", "http://localhost:3000"])
 @requires_auth
 def private():
-    return {'message': 'This API requires authorization'}
+    return {'message': 'You are authorized to access this resource'}
 
 @api_private.route('/api/private-scoped', methods=['GET', 'POST'])
 @cross_origin(headers=["Content-Type", "Authorization"])
@@ -18,14 +18,14 @@ def private():
 def private_scoped():
     if request.method == 'POST':
         if requires_scope("post:private"):
-            return {'message': 'This API requires post:private scope'}
+            return {'message': 'You have post:private permission to access this resource'}
         raise AuthError({
             "code": "Unauthorized",
             "description": "You don't have access to this resource"
         }, 403)
     else:
         if requires_scope("read:private"):
-            return {'message': 'This API requires read:private scope'}
+            return {'message': 'You have read:private permission to access this resource'}
         raise AuthError({
             "code": "Unauthorized",
             "description": "You don't have access to this resource"
